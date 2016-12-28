346717ede1511f91070a199c07dce428974c52f6
yehnra <yehnra.faith@gmail.com>
2016-12-28 02:50:47 -0700

    changed a comment

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // A simple Rust 1.14 implementation of cat using only std
 // TODO: cat arguments: -A, -e, -s -t, -v
-// TODO: move help and version text to separate files
+// TODO: move help and version text to separate files (?)
 // TODO: more comments
 // TODO: "interactive cat" (like when you run cat with no args in bash)
 // TODO: multiple files
